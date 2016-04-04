29644f8a0d6b5cf02e7b9c5b4c0bf762d6568a71
eliheuer <eliheuer@gmail.com>
2016-04-04 15:12:46 -0400

    working

--- eli_heuer_type_specimen.py
+++ eli_heuer_type_specimen.py
@@ -151,7 +151,7 @@ text("12pt", (-1, 704-(256+16)))
 
 txt_line_one="""Wristwatch Nano"""
 txt_line_two="""Render-farm"""
-txt_line_three="""range-rover hotdog apophenia wristwatch carbon nano- human silent systemic shrine physical otaku industrial grade media Shibuya. motion tiger-team warehouse sentient chrome denim rifle advert sprawl meta- tank-traps paranoid shoes systemic decay. spook camera motion vinyl wonton soup A.I. denim spook gang spook silent towards refrigerator paranoid uplink. soul-delay concrete soul-delay monofilament rifle computer human tiger-team car courier spook bicycle plastic narrative singularity. 8-bit boy nano- stimulate marketing office decay j-pop sunglasses meta- geodesic lights chrome motion construct. towards savant construct decay tiger-team -space semiotics"""
+txt_line_three="""GNU, which stands for Gnu's Not Unix, is the name for the complete Unix-compatible software system which I am writing so that I can give it away free to everyone who can use it.(1) Several other volunteers are helping me. Contributions of time, money, programs and equipment are greatly needed. So far we have an Emacs text editor with Lisp for writing editor commands, a source level debugger, a yacc-compatible parser generator, a linker, and around 35 utilities. A shell (command interpreter) is nearly completed. A new portable optimizing C compiler has compiled itself and may be released this year. An initial kernel exists but many more features are needed to emulate Unix. When the kernel and compiler are finished, it will be possible to distribute a GNU system suitable for program development. We will use TeX as our text formatter, but an nroff is being worked on. We will use the free, portable X Window System as well. After this we will add a portable Common Lisp, an Empire game, a spreadsheet, and hundreds of other things, plus online documentation. We hope to supply, eventually, everything useful that normally comes with a Unix system, and more."""
 txt_line_five="""lmnopqrs"""
 txt_line_six="""tuvwxyz"""
 txt_line_seven="""1234567890"""
@@ -167,9 +167,9 @@ textBox(txt_line_two, (-2, -32, 520, 628-80))
 fontSize(12)
 tracking(0)
 lineHeight(14)
-textBox(txt_line_three, (0, 0, 256+16, (16*27)-4))
-lineHeight(16)
-textBox(txt_line_three, (288, 0, 256+16, (16*27)-4))
+textBox(txt_line_three, (0, 0, 256, (16*26)-4))
+lineHeight(14)
+textBox(txt_line_three, (288, 0, 256, (16*26)-4))
            
 ###############################################################
 
