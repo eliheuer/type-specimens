8f107328b244194e001227592a86cb4b1c6877ce
eliheuer <eliheuer@gmail.com>
2016-04-04 22:50:13 -0400

    update

--- eli_heuer_type_specimen.py
+++ eli_heuer_type_specimen.py
@@ -24,7 +24,6 @@ def grid(origin, width, height, num_x_units, num_y_units):
     translate(*origin)
     strokeWidth(0.5)
     stroke(0.9, 0.1, 0.1, 0.1)  
-    #stroke(1)  
     fill(None)
     
     step_x = 0 
@@ -592,7 +591,7 @@ textBox(txt_line_seven, (-6, -32, 520, 628-(80*6)))
 lineHeight(None)
 ###############################################################
 
-################################################ Isotherma-Alpha
+############################################### HeuerSchrift-Regular
 newPage()
 
 # grid
@@ -615,31 +614,124 @@ text("Repo: github.com/eliheuer/Toren", (256+64, 704-16))
 text("License: SIL Open Font License v1.1", (256+64, 704-32))
 
 text("72pt", (-1, 704-80))
-text("12pt", (-1, 704-(256+16)))
 
-txt_line_one="""Wristwatch"""
-txt_line_two="""Render-farm"""
-txt_line_three="""GNU, which stands for Gnu's Not Unix, is the name for the complete Unix-compatible software system which I am writing so that I can give it away free to everyone who can use it.(1) Several other volunteers are helping me. Contributions of time, money, programs and equipment are greatly needed. So far we have an Emacs text editor with Lisp for writing editor commands, a source level debugger, a yacc-compatible parser generator, a linker, and around 35 utilities. A shell (command interpreter) is nearly completed. A new portable optimizing C compiler has compiled itself and may be released this year. An initial kernel exists but many more features are needed to emulate Unix. When the kernel and compiler are finished, it will be possible to distribute a GNU system suitable for program development. We will use TeX as our text formatter, but an nroff is being worked on. We will use the free, portable X Window System as well. After this we will add a portable Common Lisp, an Empire game, a spreadsheet, and hundreds of other things, plus online documentation. We hope to supply, eventually, everything useful that normally comes with a Unix system, and more."""
+txt_line_one="""ABCDEFGHIJ"""
+txt_line_two="""KLMNOPQRS"""
+txt_line_three="""TUVWXYZ"""
+txt_line_four="""abcdefghijk"""
 txt_line_five="""lmnopqrs"""
 txt_line_six="""tuvwxyz"""
 txt_line_seven="""1234567890"""
 
-font("Isotherma-Alpha")
+font("HeuerSchrift-Regular")
 fontSize(72)
 tracking(0)
 lineHeight(50)
 
 textBox(txt_line_one, (1, -32, 520, 628))
 textBox(txt_line_two, (-2, -32, 520, 628-80))
-
-fontSize(12)
-tracking(0)
-lineHeight(14)
-textBox(txt_line_three, (0, 0, 256, (16*26)-4))
-lineHeight(14)
-textBox(txt_line_three, (288, 0, 256, (16*26)-4))
-           
+textBox(txt_line_three, (-2, -32, 520, 628-(80*2)))
+textBox(txt_line_four, (-2, -32, 520, 628-(80*3)))
+textBox(txt_line_five, (0, -32, 520, 628-(80*4)))
+textBox(txt_line_six, (0, -32, 520, 628-(80*5)))
+textBox(txt_line_seven, (-6, -32, 520, 628-(80*6)))
+lineHeight(None)
 ###############################################################
+
+############################################### Nu-Regular
+newPage()
+
+# grid
+#translate(*origin) # grid off
+grid(origin, width, height, num_x_units*2, num_y_units*2) # grid on
+
+# type 
+fontSize(12)
+font("Helvetica Neue Bold")
+tracking(0)
+fill(0.1, 0.1, 0.1)
+stroke(None)
+text("Family Name: Toren", (-1, 704-16))
+text("Style Name: Regular", (-1, 704-32))
+
+text("Units Per Em: 1000", (128+32, 704-16))
+text("Release Date: 2014", (128+32, 704-32))
+
+text("Repo: github.com/eliheuer/Toren", (256+64, 704-16))
+text("License: SIL Open Font License v1.1", (256+64, 704-32))
+
+text("72pt", (-1, 704-80))
+
+txt_line_one="""ABCDEFGHIJ"""
+txt_line_two="""KLMNOPQRS"""
+txt_line_three="""TUVWXYZ"""
+txt_line_four="""abcdefghijk"""
+txt_line_five="""lmnopqrs"""
+txt_line_six="""tuvwxyz"""
+txt_line_seven="""1234567890"""
+
+font("Nu-Regular")
+fontSize(72)
+tracking(0)
+lineHeight(50)
+
+textBox(txt_line_one, (1, -32, 520, 628))
+textBox(txt_line_two, (-2, -32, 520, 628-80))
+textBox(txt_line_three, (-2, -32, 520, 628-(80*2)))
+textBox(txt_line_four, (-2, -32, 520, 628-(80*3)))
+textBox(txt_line_five, (0, -32, 520, 628-(80*4)))
+textBox(txt_line_six, (0, -32, 520, 628-(80*5)))
+textBox(txt_line_seven, (-6, -32, 520, 628-(80*6)))
+lineHeight(None)
+###############################################################
+
+##################################################### fony-bold
+newPage()
+
+# grid
+#translate(*origin) # grid off
+grid(origin, width, height, num_x_units*2, num_y_units*2) # grid on
+
+# type 
+fontSize(12)
+font("Helvetica Neue Bold")
+tracking(0)
+fill(0.1, 0.1, 0.1)
+stroke(None)
+text("Family Name: Toren", (-1, 704-16))
+text("Style Name: Regular", (-1, 704-32))
+
+text("Units Per Em: 1000", (128+32, 704-16))
+text("Release Date: 2014", (128+32, 704-32))
+
+text("Repo: github.com/eliheuer/Toren", (256+64, 704-16))
+text("License: SIL Open Font License v1.1", (256+64, 704-32))
+
+text("72pt", (-1, 704-80))
+
+txt_line_one="""ABCDEFGHIJ"""
+txt_line_two="""KLMNOPQRS"""
+txt_line_three="""TUVWXYZ"""
+txt_line_four="""abcdefghijk"""
+txt_line_five="""lmnopqrs"""
+txt_line_six="""tuvwxyz"""
+txt_line_seven="""1234567890"""
+
+font("Fony-Bold")
+fontSize(72)
+tracking(0)
+lineHeight(50)
+
+textBox(txt_line_one, (1, -32, 520, 628))
+textBox(txt_line_two, (-2, -32, 520, 628-80))
+textBox(txt_line_three, (-2, -32, 520, 628-(80*2)))
+textBox(txt_line_four, (-2, -32, 520, 628-(80*3)))
+textBox(txt_line_five, (0, -32, 520, 628-(80*4)))
+textBox(txt_line_six, (0, -32, 520, 628-(80*5)))
+textBox(txt_line_seven, (-6, -32, 520, 628-(80*6)))
+lineHeight(None)
+###############################################################
+
 
 ############################################### MMXIMedium-Regular
 newPage()
@@ -737,7 +829,271 @@ textBox(txt_line_three, (288, 0, 256, (16*26)-4))
            
 ###############################################################
 
-# MMXIMedium-Regular
-# MMXIMediumOblique-Regular
+######################################  MMXIMediumOblique-Regular
+newPage()
+
+# grid
+#translate(*origin) # grid off
+grid(origin, width, height, num_x_units*2, num_y_units*2) # grid on
+
+# type 
+fontSize(12)
+font("Helvetica Neue Bold")
+tracking(0)
+fill(0.1, 0.1, 0.1)
+stroke(None)
+text("Family Name: Toren", (-1, 704-16))
+text("Style Name: Regular", (-1, 704-32))
+
+text("Units Per Em: 1000", (128+32, 704-16))
+text("Release Date: 2014", (128+32, 704-32))
+
+text("Repo: github.com/eliheuer/Toren", (256+64, 704-16))
+text("License: SIL Open Font License v1.1", (256+64, 704-32))
+
+text("72pt", (-1, 704-80))
+
+txt_line_one="""ABCDEFGHIJ"""
+txt_line_two="""KLMNOPQRS"""
+txt_line_three="""TUVWXYZ"""
+txt_line_four="""abcdefghijk"""
+txt_line_five="""lmnopqrs"""
+txt_line_six="""tuvwxyz"""
+txt_line_seven="""1234567890"""
+
+font("MMXIMediumOblique-Regular")
+fontSize(72)
+tracking(0)
+lineHeight(50)
+
+textBox(txt_line_one, (1, -32, 520, 628))
+textBox(txt_line_two, (-2, -32, 520, 628-80))
+textBox(txt_line_three, (-2, -32, 520, 628-(80*2)))
+textBox(txt_line_four, (-2, -32, 520, 628-(80*3)))
+textBox(txt_line_five, (0, -32, 520, 628-(80*4)))
+textBox(txt_line_six, (0, -32, 520, 628-(80*5)))
+textBox(txt_line_seven, (-6, -32, 520, 628-(80*6)))
+lineHeight(None)
+###############################################################
+
+###################################  #MMXIMediumOblique-Regular
+newPage()
+
+# grid
+#translate(*origin) # grid off
+grid(origin, width, height, num_x_units*2, num_y_units*2) # grid on
+
+# type 
+fontSize(12)
+font("Helvetica Neue Bold")
+tracking(0)
+fill(0.1, 0.1, 0.1)
+stroke(None)
+text("Family Name: Toren", (-1, 704-16))
+text("Style Name: Regular", (-1, 704-32))
+
+text("Units Per Em: 1000", (128+32, 704-16))
+text("Release Date: 2014", (128+32, 704-32))
+
+text("Repo: github.com/eliheuer/Toren", (256+64, 704-16))
+text("License: SIL Open Font License v1.1", (256+64, 704-32))
+
+text("72pt", (-1, 704-80))
+text("12pt", (-1, 704-(256+16)))
+
+txt_line_one="""Wristwatch"""
+txt_line_two="""Render-farm"""
+txt_line_three="""GNU, which stands for Gnu's Not Unix, is the name for the complete Unix-compatible software system which I am writing so that I can give it away free to everyone who can use it.(1) Several other volunteers are helping me. Contributions of time, money, programs and equipment are greatly needed. So far we have an Emacs text editor with Lisp for writing editor commands, a source level debugger, a yacc-compatible parser generator, a linker, and around 35 utilities. A shell (command interpreter) is nearly completed. A new portable optimizing C compiler has compiled itself and may be released this year. An initial kernel exists but many more features are needed to emulate Unix. When the kernel and compiler are finished, it will be possible to distribute a GNU system suitable for program development. We will use TeX as our text formatter, but an nroff is being worked on. We will use the free, portable X Window System as well. After this we will add a portable Common Lisp, an Empire game, a spreadsheet, and hundreds of other things, plus online documentation. We hope to supply, eventually, everything useful that normally comes with a Unix system, and more."""
+txt_line_five="""lmnopqrs"""
+txt_line_six="""tuvwxyz"""
+txt_line_seven="""1234567890"""
+
+font("MMXIMediumOblique-Regular")
+fontSize(72)
+tracking(0)
+lineHeight(50)
+
+textBox(txt_line_one, (1, -32, 520, 628))
+textBox(txt_line_two, (-2, -32, 520, 628-80))
+
+fontSize(12)
+tracking(0)
+lineHeight(14)
+textBox(txt_line_three, (0, 0, 256, (16*26)-4))
+lineHeight(14)
+textBox(txt_line_three, (288, 0, 256, (16*26)-4))
+           
+###############################################################
+
+######################################  MMXIBlack-Regular
+newPage()
+
+# grid
+#translate(*origin) # grid off
+grid(origin, width, height, num_x_units*2, num_y_units*2) # grid on
+
+# type 
+fontSize(12)
+font("Helvetica Neue Bold")
+tracking(0)
+fill(0.1, 0.1, 0.1)
+stroke(None)
+text("Family Name: Toren", (-1, 704-16))
+text("Style Name: Regular", (-1, 704-32))
+
+text("Units Per Em: 1000", (128+32, 704-16))
+text("Release Date: 2014", (128+32, 704-32))
+
+text("Repo: github.com/eliheuer/Toren", (256+64, 704-16))
+text("License: SIL Open Font License v1.1", (256+64, 704-32))
+
+text("72pt", (-1, 704-80))
+
+txt_line_one="""ABCDEFGHIJ"""
+txt_line_two="""KLMNOPQRS"""
+txt_line_three="""TUVWXYZ"""
+txt_line_four="""abcdefghijk"""
+txt_line_five="""lmnopqrs"""
+txt_line_six="""tuvwxyz"""
+txt_line_seven="""1234567890"""
+
+font("MMXIBlack-Regular")
+fontSize(72)
+tracking(0)
+lineHeight(50)
+
+textBox(txt_line_one, (1, -32, 520, 628))
+textBox(txt_line_two, (-2, -32, 520, 628-80))
+textBox(txt_line_three, (-2, -32, 520, 628-(80*2)))
+textBox(txt_line_four, (-2, -32, 520, 628-(80*3)))
+textBox(txt_line_five, (0, -32, 520, 628-(80*4)))
+textBox(txt_line_six, (0, -32, 520, 628-(80*5)))
+textBox(txt_line_seven, (-6, -32, 520, 628-(80*6)))
+lineHeight(None)
+###############################################################
+
+############################################  MMXIBlack-Regular
+newPage()
+
+# grid
+#translate(*origin) # grid off
+grid(origin, width, height, num_x_units*2, num_y_units*2) # grid on
+
+# type 
+fontSize(12)
+font("Helvetica Neue Bold")
+tracking(0)
+fill(0.1, 0.1, 0.1)
+stroke(None)
+text("Family Name: Toren", (-1, 704-16))
+text("Style Name: Regular", (-1, 704-32))
+
+text("Units Per Em: 1000", (128+32, 704-16))
+text("Release Date: 2014", (128+32, 704-32))
+
+text("Repo: github.com/eliheuer/Toren", (256+64, 704-16))
+text("License: SIL Open Font License v1.1", (256+64, 704-32))
+
+text("72pt", (-1, 704-80))
+text("12pt", (-1, 704-(256+16)))
+
+txt_line_one="""Wristwatch"""
+txt_line_two="""Render-farm"""
+txt_line_three="""GNU, which stands for Gnu's Not Unix, is the name for the complete Unix-compatible software system which I am writing so that I can give it away free to everyone who can use it.(1) Several other volunteers are helping me. Contributions of time, money, programs and equipment are greatly needed. So far we have an Emacs text editor with Lisp for writing editor commands, a source level debugger, a yacc-compatible parser generator, a linker, and around 35 utilities. A shell (command interpreter) is nearly completed. A new portable optimizing C compiler has compiled itself and may be released this year. An initial kernel exists but many more features are needed to emulate Unix. When the kernel and compiler are finished, it will be possible to distribute a GNU system suitable for program development. We will use TeX as our text formatter, but an nroff is being worked on. We will use the free, portable X Window System as well. After this we will add a portable Common Lisp, an Empire game, a spreadsheet, and hundreds of other things, plus online documentation. We hope to supply, eventually, everything useful that normally comes with a Unix system, and more."""
+txt_line_five="""lmnopqrs"""
+txt_line_six="""tuvwxyz"""
+txt_line_seven="""1234567890"""
+
+font("MMXIBlack-Regular")
+fontSize(72)
+tracking(0)
+lineHeight(50)
+
+textBox(txt_line_one, (1, -32, 520, 628))
+textBox(txt_line_two, (-2, -32, 520, 628-80))
+
+fontSize(12)
+tracking(0)
+lineHeight(14)
+textBox(txt_line_three, (0, 0, 256, (16*26)-4))
+lineHeight(14)
+textBox(txt_line_three, (288, 0, 256, (16*26)-4))
+           
+###############################################################
+
+######################################  Ashley'sFont-Regular
+newPage()
+
+# grid
+#translate(*origin) # grid off
+grid(origin, width, height, num_x_units*2, num_y_units*2) # grid on
+
+# type 
+fontSize(12)
+font("Helvetica Neue Bold")
+tracking(0)
+fill(0.1, 0.1, 0.1)
+stroke(None)
+text("Family Name: Toren", (-1, 704-16))
+text("Style Name: Regular", (-1, 704-32))
+
+text("Units Per Em: 1000", (128+32, 704-16))
+text("Release Date: 2014", (128+32, 704-32))
+
+text("Repo: github.com/eliheuer/Toren", (256+64, 704-16))
+text("License: SIL Open Font License v1.1", (256+64, 704-32))
+
+text("72pt", (-1, 704-80))
+
+txt_line_one="""ABCDEFGHIJ"""
+txt_line_two="""KLMNOPQRS"""
+txt_line_three="""TUVWXYZ"""
+txt_line_four="""abcdefghijk"""
+txt_line_five="""lmnopqrs"""
+txt_line_six="""tuvwxyz"""
+txt_line_seven="""1234567890"""
+
+font("Ashley'sFont-Regular")
+fontSize(72)
+tracking(0)
+lineHeight(50)
+
+textBox(txt_line_one, (1, -32, 520, 628))
+textBox(txt_line_two, (-2, -32, 520, 628-80))
+textBox(txt_line_three, (-2, -32, 520, 628-(80*2)))
+textBox(txt_line_four, (-2, -32, 520, 628-(80*3)))
+textBox(txt_line_five, (0, -32, 520, 628-(80*4)))
+textBox(txt_line_six, (0, -32, 520, 628-(80*5)))
+textBox(txt_line_seven, (-6, -32, 520, 628-(80*6)))
+lineHeight(None)
+###############################################################
+
+######################################  colophone
+newPage()
+
+# grid
+#translate(*origin) # grid off
+grid(origin, width, height, num_x_units*2, num_y_units*2) # grid on
+
+# type 
+fontSize(12)
+font("Helvetica Neue Bold")
+tracking(0)
+fill(0.1, 0.1, 0.1)
+stroke(None)
+text("Colophon:", (-1, 704-16))
+text("git repo here: https://github.com/eliheuer/type-specimens", (-1, 704-96))
+
+
+
+
+
+font("Ashler")
+fontSize(72)
+tracking(0)
+lineHeight(50)
+
+###############################################################
+
 # MMXIBlack-Regular
 saveImage([u"~/type/type-specimens/Eli_Heuer_Type_Specimen.pdf",])
